148b50c8dc28e4f2b1c13fea9e3db661e673df28
ooreally <anu1512sri@gmail.com>
2021-02-13 13:05:24 +0530

    updated Logo

--- src/components/Instructor/Instructor.js
+++ src/components/Instructor/Instructor.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/truxam_Logo.png";
 import "./Instructor.css";
 
 export default function Instructor() {
   return (
     <div className="instructor">
       
-      <h1 className="heading main-heading"><img src="https://lh4.googleusercontent.com/1eV0cYZK4l4u2d8kpgGWSg2DUhpGkA1JOs_3Y37TDT-haHORf__qiUACpEccp69l5CFtiKHCBO4Z7D_q9ChDUF4=w16383"/> Instructor Portal</h1>
+      <h1 className="heading main-heading"><img src={logo}/> Instructor Portal</h1>
       <div className="content_main">
         <Link
           style={{ textDecoration: "none", width: "max-content" }}
